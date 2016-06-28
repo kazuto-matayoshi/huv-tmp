b9607b4cc80379006c0f80ff442a7e80fb803304
k-matayoshi <k-matayoshi@huvrid.co.jp>
2016-06-28 21:35:07 +0900

    タイトルが入らなかったバグ修正

--- wp/wp-content/themes/base/header.php
+++ wp/wp-content/themes/base/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<title><?php wp_title(); ?></title>
+		<title><?php bloginfo('name'); ?><?php wp_title('｜'); ?></title>
 
 		<!-- icon -->
 		<link rel="shortcut icon" href="/img/favicon.ico">
