c15716664f0f89d0f232b70a8f6601c1ddec5ae3
k-matayoshi <k-matayoshi@huvrid.co.jp>
2016-08-19 10:19:02 +0900

    wp4.6へupdate

--- wp/wp-content/themes/base/function/init.php
+++ wp/wp-content/themes/base/function/init.php
@@ -188,6 +188,23 @@ function remove_default_post_screen_metaboxes() {
 
 add_action('admin_menu','remove_default_post_screen_metaboxes');
 
+function remove_post_supports() {
+	// remove_post_type_support( 'post', 'title' );           // タイトル
+	// remove_post_type_support( 'post', 'editor' );          // 本文欄
+	// remove_post_type_support( 'post', 'author' );          // 作成者
+	// remove_post_type_support( 'post', 'thumbnail' );       // アイキャッチ
+	// remove_post_type_support( 'post', 'excerpt' );         // 抜粋
+	// remove_post_type_support( 'post', 'trackbacks' );      // トラックバック
+	// remove_post_type_support( 'post', 'custom-fields' );   // カスタムフィールド
+	// remove_post_type_support( 'post', 'comments' );        // コメント
+	// remove_post_type_support( 'post', 'revisions' );       // リビジョン
+	// remove_post_type_support( 'post', 'page-attributes' ); // ページ属性
+	// remove_post_type_support( 'post', 'post-formats' );    // 投稿フォーマット
+
+	// unregister_taxonomy_for_object_type( 'category', 'post' ); // カテゴリ
+	// unregister_taxonomy_for_object_type( 'post_tag', 'post' ); // タグ
+}
+add_action( 'init', 'remove_post_supports' );
 /*
  * 06.0 - 投稿画面のパーマリンク編集部分を非表示
  */
