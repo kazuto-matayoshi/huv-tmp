b349e0fc30d39a4c7082fe1d9e1accae6cb9bde7
k-matayoshi <k-matayoshi@huvrid.co.jp>
2017-01-05 11:44:26 +0900

    the_date()→echo get_the_date()に変更

--- wp/wp-content/themes/base/archive/post.php
+++ wp/wp-content/themes/base/archive/post.php
@@ -17,7 +17,7 @@ if ( have_posts() ) :
 				echo'<img src="/img/no-image.jpg" alt="no-image">';
 			}
 		?></p>
-		<p class="post-day"><?php the_date(); ?></p>
+		<p class="post-day"><?php echo get_the_date(); ?></p>
 		<p class="post-ttl"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></p>
 	</li>
 <?php
