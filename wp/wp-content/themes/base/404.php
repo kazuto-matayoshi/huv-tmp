37400b25481c6dc9f448d7f532ed60986edae96b
k-matayoshi <k-matayoshi@huvrid.co.jp>
2017-04-18 23:49:52 +0900

    404調整

--- wp/wp-content/themes/base/404.php
+++ wp/wp-content/themes/base/404.php
@@ -1,11 +1,3 @@
 <?php get_header(); ?>
-	<div class="error">
-		<p class="error-img">
-			<img src="<?php echo get_template_directory_uri(); ?>/img/404/attention.png" alt="attention">
-		</p>
-		<p class="error-txt">指定されたURLは存在しませんでした。</p>
-		<p class="error-btn">
-			<a href="/">TOPへ</a>
-		</p>
-	</div>
+<?php get_template_part( '404_content' ); ?>
 <?php get_footer(); ?>
