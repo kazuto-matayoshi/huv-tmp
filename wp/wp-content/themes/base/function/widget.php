6ebf7c25b3478efce1f556770b9dd5fc51a965f9
k-matayoshi <k-matayoshi@huvrid.co.jp>
2017-04-25 12:40:37 +0900

    インデント等々の調整

--- wp/wp-content/themes/base/function/widget.php
+++ wp/wp-content/themes/base/function/widget.php
@@ -16,35 +16,62 @@ function my_css_attributes_filter($var) {
  * - いらないデフォルトウィジェットを消す -
  */
 
-function init_widgets(){
-	unregister_widget('WP_Widget_RSS');             // RSS/Atom フィード
-	unregister_widget('WP_Widget_Meta');            // メタ情報(login/outなど)
-	unregister_widget('WP_Widget_Text');            // 任意のテキストやHTMLを挿入するウィジェット
-	unregister_widget('WP_Widget_Pages');           // 固定ページ一覧用のウィジェット
-	unregister_widget('WP_Widget_Links');           // リンク集
-	unregister_widget('WP_Widget_Search');          // サイト内検索フォーム
-	// unregister_widget('WP_Nav_Menu_Widget');        // カスタムメニュー
-	unregister_widget('WP_Widget_Archives');        // 投稿の月別アーカイブ
-	unregister_widget('WP_Widget_Calendar');        // カレンダー
-	unregister_widget('WP_Widget_Tag_Cloud');       // タグクラウド
-	unregister_widget('WP_Widget_Categories');      // カテゴリーリスト
-	unregister_widget('WP_Widget_Recent_Posts');    // 最近の投稿
-	unregister_widget('WP_Widget_Recent_Comments'); // 最近のコメント
+function unregister_widgets(){
+	// RSS/Atom フィード
+	unregister_widget('WP_Widget_RSS');
+
+	// メタ情報(login/outなど)
+	unregister_widget('WP_Widget_Meta');
+
+	// 任意のテキストやHTMLを挿入するウィジェット
+	// unregister_widget('WP_Widget_Text');
+
+	// 固定ページ一覧用のウィジェット
+	unregister_widget('WP_Widget_Pages');
+
+	// リンク集
+	unregister_widget('WP_Widget_Links');
+
+	// サイト内検索フォーム
+	unregister_widget('WP_Widget_Search');
+
+	// カスタムメニュー
+	unregister_widget('WP_Nav_Menu_Widget');
+
+	// 投稿の月別アーカイブ
+	unregister_widget('WP_Widget_Archives');
+
+	// カレンダー
+	unregister_widget('WP_Widget_Calendar');
+
+	// タグクラウド
+	unregister_widget('WP_Widget_Tag_Cloud');
+
+	// カテゴリーリスト
+	unregister_widget('WP_Widget_Categories');
+
+	// 最近の投稿
+	unregister_widget('WP_Widget_Recent_Posts');
+
+	// 最近のコメント
+	unregister_widget('WP_Widget_Recent_Comments');
 }
-add_action('widgets_init', 'init_widgets');
+add_action('widgets_init', 'unregister_widgets');
 
 
 /**
  * - ウィジェット管理画面右側のボックス -
  */
+add_action( 'widgets_init', function () {
 
 register_sidebar( array(  //「サイドバー」を登録する
 	'name'          => __( 'グローバルナビゲーション' ),
 	'id'            => 'gnav',
-	'description' => __( 'グローバルナビゲーションの追加' ),
+	'description'   => __( 'グローバルナビゲーションの追加' ),
 	'before_widget' => '',
 	'after_widget'  => '',
 	'before_title'  => '',
 	'after_title'   => '',
 ) );
 
+});
