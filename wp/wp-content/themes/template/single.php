2b98f7f07f6b56a5e2e58832a1dd2fdac61943af
k-matayoshi <k-matayoshi@huvrid.co.jp>
2017-07-25 20:49:41 +0900

    singleのpathの取得方法調整

--- wp/wp-content/themes/template/single.php
+++ wp/wp-content/themes/template/single.php
@@ -1,7 +1,28 @@
 <?php
 get_header();
 
-  $path = get_post_type($post->ID);
+  // $path = get_post_type($post->ID);
+
+  $archive_link = get_post_type_archive_link( get_post_type() );
+  $protocol     = empty($_SERVER["HTTPS"]) ? "http://" : "https://";
+  $domain       = $_SERVER['HTTP_HOST'];
+  $archive_dir  = str_replace( $protocol.$domain, '', $archive_link );
+
+  $url  = explode( '/', $archive_dir );
+  $path = '';
+
+  if ( get_post_type() === $url[1] ) {
+    $path = get_post_type() ? get_post_type() : 'post';
+  } else {
+    // $path = $url[1];
+    foreach ( $url as $key => $value ) {
+      if ( $value && !$path ) {
+        $path .= $value;
+      } elseif ( $value && $path ) {
+        $path .= '-'.$value;
+      }
+    }
+  }
 
   // 書き出しテスト
   // echo 'single : '.$path;
