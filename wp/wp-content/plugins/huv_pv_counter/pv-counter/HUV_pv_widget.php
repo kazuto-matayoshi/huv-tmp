b9f024071d1b46466f636222c7d03a6cb7326ab5
k-matayoshi <k-matayoshi@huvrid.co.jp>
2018-02-07 13:31:00 +0900

    Revert "PVカウンター"
    This reverts commit eb38387f1cc8903a1141077ba15af1a0303db0c0.

--- wp/wp-content/plugins/huv_pv_counter/pv-counter/HUV_pv_widget.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * - ウィジェットの追加 -
- */
-require( dirname( __FILE__ ) . '/widgets/HUV_pv_archives.php' );
-
-add_action( 'widgets_init', function () {
-  register_widget( 'HUV_pv_archives' );
-});
