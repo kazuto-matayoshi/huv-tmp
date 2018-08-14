94e760a19f29e193301037e44717773d1cc1b576
k-matayoshi <k-matayoshi@huvrid.co.jp>
2018-08-14 13:59:58 +0900

    smooth scroll追加

--- wp/wp-content/themes/template/assets/js/common.js
+++ wp/wp-content/themes/template/assets/js/common.js
@@ -1,4 +1,28 @@
 (function($){
   console.log('common');
   // var swiper = new Swiper('.mv');
+
+  /* smooth scroll */
+  $('a[href *= "#"]').on('click',function(){
+    var speed     = 700;
+
+    var target    = $(this).attr('href').split('#');
+    target        = target ? "#" + target[1] : "#";
+
+    if ( target !== "#" && $( target ).length === 0 ) {
+      return;
+    }
+
+    var targetTop = target != "#" ? $( "#" + target[1] ).offset().top : 0;
+
+    $('html,body').stop().animate({
+        scrollTop: targetTop,
+      },
+      {
+        'duration': speed,
+        'easing': 'swing',
+    });
+
+    return false;
+  });
 })(jQuery);
